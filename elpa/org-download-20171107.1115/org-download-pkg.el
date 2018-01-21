317d5e260648b6f683befba3977fcf43ad71590f
RioZRon <17323844268@163.com>
2018-01-21 14:26:17 +0800

    MoveToWin

---
+++ elpa/org-download-20171107.1115/org-download-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-download" "20171107.1115" "Image drag-and-drop for Emacs org-mode" '((async "1.2")) :commit "c75095778c02e70fc7efa2aac5c77936b56d04db" :url "https://github.com/abo-abo/org-download" :keywords '("images" "screenshots" "download"))
