0e193eee9f18c5f5d40bd7b01a28d6a50e58d0d4
RioZRon <17323844268@163.com>
2018-01-21 12:09:34 +0800

    MoveToWin

---
+++ custom.el
@@ -0,0 +1,37 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-timeout 5)
+ '(company-tooltip-minimum-width 3)
+ '(eww-download-directory "~/Downloads/EmacasEwwDowloads")
+ '(eww-history-limit 500)
+ '(eww-search-prefix "Http://www.baidu.com/s?ie=UTF-8&wd=")
+ '(eww-use-external-browser-for-content-type nil)
+ '(org-agenda-span (quote day))
+ '(org-closed-keep-when-no-todo nil)
+ '(org-priority-faces nil)
+ '(org-todo-keyword-faces
+   (quote
+    (("HOLD" . "yellow")
+     ("LIST" . "write")
+     ("DONE" . "black")
+     ("TODO" . "red")
+     ("NEXT" . "skyblue")
+     ("WAIT" . "write"))))
+ '(org-todo-keywords
+   (quote
+    ((sequence "WAIT" "NEXT" "TODO" "DONE")
+     (sequence "HOLD")
+     (sequence "LIST" "NEXT" "DONE"))))
+ '(package-selected-packages
+   (quote
+    (yaml-mode window-numbering livid-mode skewer-mode json-mode js2-refactor multiple-cursors company-tern dash-functional web-beautify simple-httpd json-snatcher json-reformat js2-mode js-doc tern coffee-mode sql-indent helm-themes helm-swoop helm-projectile helm-mode-manager helm-flx helm-descbinds helm-ag ace-jump-helm-line web-mode tagedit slim-mode scss-mode sass-mode pug-mode less-css-mode haml-mode emmet-mode company-web web-completion-data powerline spinner hydra parent-mode helm helm-core flx smartparens iedit anzu evil goto-chg undo-tree highlight f dash s diminish projectile pkg-info epl counsel swiper ivy bind-map bind-key packed async avy popup nyan-mode xterm-color unfill smeargle shell-pop orgit org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-download mwim multi-term mmm-mode markdown-toc markdown-mode magit-gitflow htmlize gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md fuzzy flyspell-correct-ivy flyspell-correct flycheck-pos-tip pos-tip flycheck evil-magit magit magit-popup git-commit with-editor eshell-z eshell-prompt-extras esh-help company-statistics company auto-yasnippet yasnippet auto-dictionary ac-ispell auto-complete zenburn-emacs-theme ws-butler winum which-key wgrep volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline smex restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint ivy-hydra info+ indent-guide hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-make google-translate golden-ratio flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu elisp-slime-nav dumb-jump define-word counsel-projectile column-enforce-mode clean-aindent-mode auto-highlight-symbol auto-compile aggressive-indent adaptive-wrap ace-window ace-link)))
+ '(tramp-syntax (quote default) nil (tramp)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:foreground "#DCDCCC" :background "#3F3F3F")))))
